dfef3ee1efcccbe07153df670b2a615bc28e121c
arifvtti <arif.vtti@gmail.com>
2023-03-09 07:58:54 +0600

    for loop

--- basic.dart
+++ basic.dart
@@ -1,12 +1,5 @@
 void main() {
-  var a = 30;
-  var b = 20;
-
-  if (a > b) {
-    print("a is greater than b");
-  } else if (b > a) {
-    print("b is greater than a");
-  } else {
-    print("a is equal to b");
+  for (var i = 1; i <= 10; i++) {
+    print("$i");
   }
 }
